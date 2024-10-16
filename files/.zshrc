2dea717194944028303a2c78c857689513f628df
Luis Diaz <machadodiazluis@gmail.com>
2024-10-15 20:37:04 -0400

    feat: add support for a fixed arch linux installation.

--- files/.zshrc
+++ files/.zshrc
@@ -1,13 +1,3 @@
-# Fix the Java Problem
-export _JAVA_AWT_WM_NONREPARENTING=1
-
-# Enable Powerlevel10k instant prompt. Should stay at the top of ~/.zshrc.
-if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-fi
-
-# Set up the prompt
-
 autoload -Uz promptinit
 promptinit
 prompt adam1
@@ -26,12 +16,12 @@ HISTFILE=~/.zsh_history
 autoload -Uz compinit
 compinit
 
+eval "$(dircolors -b)"
 zstyle ':completion:*' auto-description 'specify: %d'
 zstyle ':completion:*' completer _expand _complete _correct _approximate
 zstyle ':completion:*' format 'Completing %d'
 zstyle ':completion:*' group-name ''
 zstyle ':completion:*' menu select=2
-eval "$(dircolors -b)"
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 zstyle ':completion:*' list-colors ''
 zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
@@ -40,23 +30,22 @@ zstyle ':completion:*' menu select=long
 zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
 zstyle ':completion:*' use-compctl false
 zstyle ':completion:*' verbose true
-
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
 zstyle ':completion:*:kill:*' command 'ps -u $USER -o pid,%cpu,tty,cputime,cmd'
-source ~/.powerlevel10k/powerlevel10k.zsh-theme
 
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ -f ~/.p10k.zsh ]] && source ~/.p10k.zsh
+# FZF source
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
-# Manual configuration
+# Powerlevel10k settings
+# (( ! ${+functions[p10k-instant-prompt-finalize]} )) || p10k-instant-prompt-finalize
+# source ~/.powerlevel10k/powerlevel10k.zsh-theme
+# [[ -f ~/.p10k.zsh ]] && source ~/.p10k.zsh
 
-PATH=/root/.local/bin:/snap/bin:/usr/sandbox/:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games:/usr/share/games:/usr/local/sbin:/usr/sbin:/sbin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games
-
-# General
+# General aliases
 alias full='sudo pacman -Syu'
 alias add='sudo pacman -S'
 
-# Poetry
+# Poetry aliases
 alias dshell='poetry run python manage.py shell'
 alias runserver='poetry run start'
 alias runbot='poetry run bot'
@@ -66,25 +55,27 @@ alias makemigrations='poetry run makemigrations'
 alias migrateall='poetry run migrate_all'
 alias makemessages='poetry run makemessages'
 alias compilemessages='poetry run compilemessages'
-alias format='poetry run black . & make format'
+alias format='poetry run pre-commit run --color=always --all-files'
 
-# Manual aliases
+# Helpful aliases
+alias ls='lsd --group-dirs=first'
+alias l='lsd --group-dirs=first'
 alias ll='lsd -lh --group-dirs=first'
 alias la='lsd -a --group-dirs=first'
-alias l='lsd --group-dirs=first'
 alias lla='lsd -lha --group-dirs=first'
-alias ls='lsd --group-dirs=first'
 alias cat='bat'
 
+alias zen="zeditor"
+
 # Django aliases
-alias django="django-admin"
-alias dj="python manage.py"
+alias django="poetry run django-admin"
+alias dj="poetry run python manage.py"
 alias djm="dj makemigrations && dj migrate"
 alias djr="dj runserver"
 alias djmr="djm && djr"
 
 # Docker
-alias depie="docker compose up -d"
+alias depie="docker compose up"
 
 # No more cd
 alias ..='cd ..'
@@ -93,9 +84,7 @@ alias ....='cd ../../..'
 alias .....='cd ../../../..'
 alias ......='cd ../../../../..'
 
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-
-# Plugins
+# ZSH Plugins
 source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 source /usr/share/zsh/plugins/zsh-sudo/sudo.plugin.zsh
@@ -108,46 +97,15 @@ function rmk(){
 
 # fzf improvement
 function fzf-lovely(){
-
-	if [ "$1" = "h" ]; then
-		fzf -m --reverse --preview-window down:20 --preview '[[ $(file --mime {}) =~ binary ]] &&
- 	                echo {} is a binary file ||
-	                 (bat --style=numbers --color=always {} ||
-	                  highlight -O ansi -l {} ||
-	                  coderay {} ||
-	                  rougify {} ||
-	                  cat {}) 2> /dev/null | head -500'
-
-	else
-	        fzf -m --preview '[[ $(file --mime {}) =~ binary ]] &&
-	                         echo {} is a binary file ||
-	                         (bat --style=numbers --color=always {} ||
-	                          highlight -O ansi -l {} ||
-	                          coderay {} ||
-	                          rougify {} ||
-	                          cat {}) 2> /dev/null | head -500'
-	fi
+	fzf -m --reverse --preview-window down:20 --preview '[[ $(file --mime {}) =~ binary ]] &&
+		echo {} is a binary file ||
+		(bat --style=numbers --color=always {} ||
+		highlight -O ansi -l {} ||
+		coderay {} ||
+		rougify {} ||
+		cat {}) 2> /dev/null | head -500'
 }
-
-function mkt(){
-	mkdir {nmap,content,exploits,scripts}
-}
-
-# Extract nmap information
-function extractPorts(){
-	ports="$(cat $1 | grep -oP '\d{1,5}/open' | awk '{print $1}' FS='/' | xargs | tr ' ' ',')"
-	ip_address="$(cat $1 | grep -oP '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}' | sort -u | head -n 1)"
-	echo -e "\n[*] Extracting information...\n" > extractPorts.tmp
-	echo -e "\t[*] IP Address: $ip_address"  >> extractPorts.tmp
-	echo -e "\t[*] Open ports: $ports\n"  >> extractPorts.tmp
-	echo $ports | tr -d '\n' | xclip -sel clip
-	echo -e "[*] Ports copied to clipboard\n"  >> extractPorts.tmp
-	cat extractPorts.tmp; rm extractPorts.tmp
-}
-
-# Finalize Powerlevel10k instant prompt. Should stay at the bottom of ~/.zshrc.
-(( ! ${+functions[p10k-instant-prompt-finalize]} )) || p10k-instant-prompt-finalize
-source ~/.powerlevel10k/powerlevel10k.zsh-theme
 
 # Poetry configuration
 PATH="$HOME/.local/bin:$PATH"
+PATH=/root/.local/bin:/snap/bin:/usr/sandbox/:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games:/usr/share/games:/usr/local/sbin:/usr/sbin:/sbin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games
--- installation.sh
+++ installation.sh
@@ -1,135 +1,127 @@
-# !/bin/bash
+#!/bin/bash
+
+function partition_disks() {
+    # TODO: make it dynamic
+    lsblk -f /dev/nvme0n1
+
+    echo "You need to partition the disks now."
+    echo "Please use `cfdisk` to partition the disk. When you are finished, press Enter to continue."
+
+    cfdisk /dev/nvme0n1
+
+    read -p "Press Enter to continue with the installation process after partitioning is complete..."
+
+    mkfs.fat -F32 -n "UEFI" /dev/nvme0n1p1
+    mkfs.btrfs -f -L "root" -R free-space-tree -n 32k /dev/nvme0n1p2
+}
+
+function install_arch_system() {
+    partition_disks
+
+    mount /dev/nvme0n1p2 /mnt
+    mount /dev/nvme0n1p1 /mnt/efi
+
+    pacstrap /mnt base{,-devel} linux{,-{headers,firmware}} grub os-prober efibootmgr \
+    ntfs-3g nano gvfs-{mtp,afc,gphoto2} xdg-user-dirs intel-ucode amd-ucode usbutils openresolv
+
+    genfstab -U /mnt >> /mnt/etc/fstab
+    arch-chroot /mnt
+
+    # Localization
+    echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen && locale-gen
+    echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+
+    # Network
+    echo arch > /etc/hostname
+
+    HOSTS="""
+    127.0.0.1    localhost
+    ::1          localhost
+    127.0.1.1    arch.localdomain   arch
+    """
+    echo "$HOSTS" > /etc/hosts
+
+    # Recreate images and install grub bootloader
+    echo "Recreating initramfs images..."
+    mkinitcpio -p
+
+    echo "Installing GRUB bootloader..."
+    grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id='Arch Linux'
+
+    read -p "Do you want to detect any OS installed on other partitions? (y/N)? " detect_os
+
+    if [[ "$detect_os" =~ ^[Yy]$ ]]; then
+        GRUB_FILE="/etc/default/grub"
+        sudo cp "$GRUB_FILE" "$GRUB_FILE.bak"
+
+        if grep -q '^#GRUB_DISABLE_OS_PROBER' "$GRUB_FILE"; then
+            echo "Uncommenting 'os-prober' option..."
+            sudo sed -i 's/^#GRUB_DISABLE_OS_PROBER/GRUB_DISABLE_OS_PROBER/' "$GRUB_FILE"
+        else
+            echo "'os-prober' option already uncommented."
+        fi
+        echo "Updating GRUB configuration to detect other operating systems..."
+        sudo grub-mkconfig -o /boot/grub/grub.cfg
+    else
+        echo "Skipping GRUB configuration update for detecting other operating systems."
+        grub-mkconfig -o /boot/grub/grub.cfg
+    fi
+
+    # Define groups and users
+    read -p "Enter the username to create: " username
+    GROUPS="power,wheel,storage,input,video,audio"
+    SHELL="/bin/bash"
+
+    if useradd -mG $GROUPS -s $SHELL "$username"; then
+        echo "User '$username' created and added to groups: $GROUPS"
+    else
+        echo "Failed to create user '$username'."
+        return 1
+    fi
+
+    if passwd "$username"; then
+        echo "Password set for user '$username'."
+    else
+        echo "Failed to set password for user '$username'."
+        return 1
+    fi
+}
 
 function install_environment() {
     # Update and Upgrade System
-    sudo pacman -Syu --noconfirm
-    sudo pacman -S kitty zsh zsh-syntax-highlighting zsh-autosuggestions lsd bat git wget nano zip --noconfirm
+    sudo pacman -Sy --needed archlinux-keyring && sudo pacman -Su --noconfirm
 
-    # Change default $SHELL
-    sudo chsh -s /usr/bin/zsh
-    sudo chsh -s /usr/bin/zsh root
+    # Install blackarch keyrings
+    read -p "Do you want to install the BlackArch Keyrings? (y/N): " blackarch
+    if [[ "$blackarch" =~ ^[Yy]$ ]]; then
+        wget -o ~/.repos/strap.sh https://blackarch.org/strap.sh && chmod +x strap.sh && sudo ./strap.sh
+    fi
 
-    # Download Arch Linux dotfiles
-    git clone https://github.com/ilyvsc/arch-linux-installation.git ~/installation
+    sudo pacman -S git wget zip openssh lsd bat fzf \
+        zsh zsh-syntax-highlighting zsh-autosuggestions --noconfirm
 
     # Setup custom .zshrc and restart SHELL
-    cp ~/installation/files/.zshrc ~/.zshrc
+    cp ~/installation/files/.zshrc ~/.zshrc && \
     sudo ln -s -f ~/.zshrc /root/.zshrc
 
-    # Install powerlevel10k
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /root/.powerlevel10k
-    cp ~/installation/files/.p10k.zsh ~/.p10k.zsh
-    sudo ln -s -f ~/.p10k.zsh /root/.p10k.zsh
-
-    # Install zsh-sudo
+    # Install zsh-sudo plugin from ohmyzsh
     sudo mkdir /usr/share/zsh/plugins/zsh-sudo
     sudo wget -O /usr/share/zsh/plugins/zsh-sudo/sudo.plugin.zsh \
         https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/sudo/sudo.plugin.zsh
 
-    # Install fzf
-    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf; ~/.fzf/install
+    # Change default $SHELL
+    chsh -s /usr/bin/zsh $USER
+    sudo chsh -s /usr/bin/zsh root
 
-    # Setup Kitty terminal
-    cp -rf ~/installation/files/config/kitty ~/.config/
-    rm -rf ~/installation
+    mkdir ~/.repos && cd ~/.repos
 
-    # Install keyrings and packages managers
-    mkdir ~/.repos; cd ~/.repos
-    sudo pacman -S --needed base-devel
-
-    # Install AUR
-    git clone https://aur.archlinux.org/paru.git; cd paru; makepkg -si
+    # Install `paru-git`
+    git clone https://aur.archlinux.org/paru-git.git && cd paru-git && makepkg -si
     paru -Syu --noconfirm
-
-    # Install blackarch keyrings
-    wget -o ~/.repos/strap.sh https://blackarch.org/strap.sh; chmod +x strap.sh; sudo ./strap.sh
 }
 
-function install_fonts() {
-    # Create and Download Fonts
-    directory="/usr/local/share/fonts"
-
-    if [ ! -d "$directory" ]; then
-        sudo mkdir -p $directory
-    fi
-
-    # Install Nerd Fonts
-    declare -a fonts=("Hack" "Iosevka" "Meslo")
-
-    for font in "${fonts[@]}"; do
-        sudo wget -O "$directory/$font.zip" "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/$font.zip" && \
-        sudo unzip "$directory/$font.zip" -d "$directory/$font" && \
-        sudo rm "$directory/$font.zip"
-    done
-
-    # Install FiraCode
-    sudo wget -O "$directory/Fira_Code.zip" "https://github.com/tonsky/FiraCode/releases/download/6.2/Fira_Code_v6.2.zip" && \
-    sudo unzip "$directory/Fira_Code.zip" -d "$directory/Fira_Code" && \
-    sudo rm "$directory/Fira_Code.zip"
-
-    # Install Noto fonts
-    paru -S noto-fonts --noconfirm
-}
-
-function install_apps() {
-    # Install necessary applications
-    paru -S 1password telegram-desktop brave-bin vivaldi visual-studio-code-bin spotify --noconfirm
-
-    # Install extra applications
-    packages="beekeeper-studio obs-studio discord slack-desktop zoom"
-
-    # Prompt user for installation confirmation
-    read -p "Do you want to install the following packages: $packages? (Y/n): " confirm
-
-    if [ "$confirm" == "Y" ] || [ "$confirm" == "y" ]; then
-        paru -S $packages --noconfirm
-
-        echo "Packages installed successfully."
-    else
-        echo "Installation aborted."
-    fi
-}
-
-function install_dev() {
-    # Setup Git config
-    git config --global user.name "Luis Diaz"
-    git config --global user.email "machadodiazluis@gmail.com"
-    git config --global init.defaultBranch main
-
-    # Install Docker
-    sudo pacman -S docker docker-compose --noconfirm
-    paru -S docker-desktop --noconfirm
-    sudo systemctl enable docker
-    sudo usermod -aG docker $USER
-
-    # Install Poetry
-    curl -sSL https://install.python-poetry.org | python3 -
-
-    if [ "$confirm" == "Y" ] || [ "$confirm" == "y" ]; then
-        paru -S jetbrains-toolbox github-cli --noconfirm
-        sudo pacman -S tree htop neofetch python python-pip --noconfirm
-
-        echo "Development tools installed successfully."
-    else
-        echo "Installation aborted."
-    fi
-}
-
-function install_desktop(){
-    # Install desktop environment
-    pacman -S gnome xorg-xprop gnome-{tweaks,browser-connector,shell-extensions} --noconfirm
-    paru -S extension-manager-git --noconfirm
-    systemctl enable gdm; systemctl start gdm
-}
-
-function main() {
+bash ./wifi.sh
+if [ $? -eq 0 ]; then
+    install_arch_system
     install_environment
-    install_fonts
-    install_apps
-    install_dev
-    install_desktop
-}
-
-main
-zsh
+fi
